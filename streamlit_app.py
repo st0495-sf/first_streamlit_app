56060479ab440968a4487015a4356b9491989dd7
st0495-sf <142996513+st0495-sf@users.noreply.github.com>
2023-08-23 15:47:22 +0100

    Update streamlit_app.py
    fix syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents' New Healthy Diner')
+streamlit.title('My New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
